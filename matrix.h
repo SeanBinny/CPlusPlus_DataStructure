3befcce3f72f03123a4a5757c8e44979d142cebc
SeanBinny <1114205253@qq.com>
2018-04-20 22:00:29 +0800

    add some note

--- matrix.h
+++ matrix.h
@@ -12,11 +12,13 @@ class Matrix
 public:
     Matrix(int theRows = 0, int theColums = 0);
     Matrix(const Matrix<T>&);
-   ~Matrix() {delete [] element;}
+   ~Matrix() {delete [] element;}                                          /* delete array space                                 */
+
     int rows() const {return theRows;}
     int columns() const {return theColumns;}
-    T&  operator () (int i, int j) const;///////////////
 
+    /*********** overloaded function *************************************/
+    T&  operator () (int i, int j) const;
     Matrix<T>& operator = (const Matrix<T>&);
     Matrix<T>  operator + (const Matrix<T>&);
     Matrix<T>  operator + () const;
@@ -26,11 +28,17 @@ public:
     Matrix<T>& operator+= (const T&);
 
 private:
-    int theRows,
-        theColumns;
-    T  *element;
+    int theRows,                                                            /* number of rows                                    */
+        theColumns;                                                         /* number of columns                                 */
+    T  *element;                                                            /* point to a array where saves elements             */
 };
 
+/***************************************************************************
+* Name          : Matrix
+* Descirpyion   : construct a matrix
+* Input         : 1.theRows : number of rows 2.theColums : number of columns
+* Output        : none
+***************************************************************************/
 template<class T>
 Matrix<T>::Matrix(int theRows, int theColums)
 {
@@ -39,31 +47,44 @@ Matrix<T>::Matrix(int theRows, int theColums)
     if ((theRows == 0 || theColums == 0)
       &&(theRows != 0 || theColums != 0))
     {
-        throw illegalParameterValue("Either noth or neither rows and"
+        throw illegalParameterValue("Either both or neither rows and"       /* eg: a[0][4] b[4][0] c[0][0]  are all wrong        */
                                     "columns should be zero");
     }
+
     this->theRows    = theRows;
     this->theColumns = theColums;
-    this->element    = new T [theRows * theColums];
+    this->element    = new T [theRows * theColums];                         /* apply for array to save elements                  */
 }
 
+/***************************************************************************
+* Name          : Matrix
+* Descirpyion   : using another matrix to construct a matrix
+* Input         : 1.m : another matrix you want to copy
+* Output        : none
+***************************************************************************/
 template <class T>
 Matrix<T>::Matrix(const Matrix<T>& m)
 {
     theRows    = m.theRows;
     theColumns = m.theColumns;
-    element    = new T [theRows * theColums];
+    element    = new T [theRows * theColumns];                              /* apply for memory                                  */
 
-    copy(m.element, m.element+theRows*theColumns,
+    copy(m.element, m.element+theRows*theColumns,                           /* copy elements                                     */
          element);
 }
 
+/***************************************************************************
+* Name          : operator =
+* Descirpyion   : overload = to ensure usage is consistent with math
+* Input         : 1.m : another matrix you use to assign
+* Output        : 1   : return a same matrix
+***************************************************************************/
 template <class T>
 Matrix<T>& Matrix<T>::operator =(const Matrix<T>& m)
 {
-    if (this != &m)
+    if (this != &m)                                                         /* only this and m are different can achieve         */
     {
-        delete [] element;
+        delete [] element;                                                  /* delete original array                             */
         theRows    = m.theRows;
         theColumns = m.theColumns;
         element    = new T [theRows * theColums];
@@ -71,22 +92,34 @@ Matrix<T>& Matrix<T>::operator =(const Matrix<T>& m)
         copy(m.element, m.element+theRows*theColumns,
              element);
     }
-    return *this;
+    return *this;                                                           /* whatever if assign successfully, return this      */
 }
 
+/***************************************************************************
+* Name          : operator ()
+* Descirpyion   : overload () to ensure usage is consistent with math
+* Input         : 1.i : row of index   2.j  :  column of index
+* Output        : 1.element  : return an element whos index is (i,j)
+***************************************************************************/
 template <class T>
 T& Matrix<T>::operator ()(int i, int j) const
 {
-    if (i < 1 || i > theRows ||  j < 1 || j > theColumns)
-        throw illegalParameterValue("Matrix index out of bounds");
+    if (i < 1 || i > theRows ||  j < 1 || j > theColumns)                   /* ensure it is not eg: a[0][4] b[4][0] c[0][0]     */
+        throw illegalParameterValue("Matrix index out of bounds");          /* or out of range                                  */
 
-    return element[(i-1)*theColumns + j - 1];
+    return element[(i-1)*theColumns + j - 1];                               /* array mapping formula                            */
 }
 
+/***************************************************************************
+* Name          : operator +
+* Descirpyion   : overload + to ensure usage is consistent with math
+* Input         : 1.m  : another matrix you want to add
+* Output        : 1.w  : return a summation matrix
+***************************************************************************/
 template <class T>
 Matrix<T> Matrix<T>::operator + (const Matrix<T>& m) const
 {
-    if (theRows != m.theRows || theColumns != m.theColumns)
+    if (theRows != m.theRows || theColumns != m.theColumns)                /* size not match                                     */
         throw illegalParameterValue("Size doesn't match");
 
     Matrix<T> w(theRows, theColumns);
@@ -96,21 +129,27 @@ Matrix<T> Matrix<T>::operator + (const Matrix<T>& m) const
     return w;
 }
 
+/***************************************************************************
+* Name          : operator *
+* Descirpyion   : overload * to ensure usage is consistent with math
+* Input         : 1.m  : another matrix you want to multiply
+* Output        : 1.w  : return a product matrix
+***************************************************************************/
 template <class T>
 Matrix<T> Matrix<T>::operator *(const Matrix<T>& m)
 {
-    if (theColumns != m.theRows)
+    if (theColumns != m.theRows)                                           /* size not match                                     */
         throw illegalParameterValue("Size doesn't match");
 
-    Matrix<T> w(theRows, m.theColumns);
+    Matrix<T> w(theRows, m.theColumns);                                    /* define a result matrix                             */
 
-    int ct = 0, cm = 0, cw = 0;
+    int ct = 0, cm = 0, cw = 0;                                            /* they are cursor of   *this   m    w                */
     for (int i = 1; i <= theRows; i++)
     {
         for (int j = 1; j <= m.theColumns; j++)
         {
-            T sum = element[ct]*m.element[cm];
-            for (int k = 2; k <= theColumns; k++)
+            T sum = element[ct]*m.element[cm];                             /* accumulate first item                              */
+            for (int k = 2; k <= theColumns; k++)                          /* accumulate all remaining items, so it start from 2 */
             {
                 ct++;
                 cm  += m.theColumns;
